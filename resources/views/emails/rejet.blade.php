ae2a1ad776c07867131ab6e85692b364377f7c95
Eddy Harold <donfackeddy12@gmail.com>
2021-11-10 10:41:18 +0100

    Update up

--- resources/views/emails/rejet.blade.php
+++ resources/views/emails/rejet.blade.php
@@ -2,17 +2,17 @@
 <html lang="fr">
 
 <head>
-  <meta charset="utf-8">
-  <title>Refus de votre projet {{$projet['intitule']}}</title>
+    <meta charset="utf-8">
+    <title>Refus de votre projet {{ $projet['intitule'] }}</title>
 </head>
 
 <body>
-  <p>Cher <strong>{{$projet['user_data']['nom_complet']}}</strong>,</p>
-  <p>Malheureusement, votre projet ne répond pas aux critères requis pour être retenu sur notre plateforme.<br />
-    Pour plus de détails, vous pouvez contacter un conseiller<br />
-    Au plaisir de travailler avec vous dans un avenir proche!</p>
-  <p><strong>Bonne chance!</strong></p>
-  <p><strong><em>Entreprendre et investir autrement!</em></strong></p>
+    <p>Cher <strong>{{ $projet['user_data']['nom_complet'] }}</strong>,</p>
+    <p>Malheureusement, votre projet ne répond pas aux critères requis pour être retenu sur notre plateforme.<br />
+        Pour plus de détails, vous pouvez contacter un conseiller<br />
+        Au plaisir de travailler avec vous dans un avenir proche!</p>
+    <p><strong>Bonne chance!</strong></p>
+    <p><strong><em>Entreprendre et investir autrement!</em></strong></p>
 </body>
 
 </html>
