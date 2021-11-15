182b7b4e33b2704688a343f224296a1570635798
Eddy Harold <donfackeddy12@gmail.com>
2021-11-15 08:53:38 +0100

    Update App

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Intervention\Image\ImageServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
         Dirape\Token\TokenServiceProvider::class,
+        Kreait\Laravel\Firebase\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
