b47c02a5a12f36065363f8a9626361eb5a3efafa
GabinNANA <gabinnana@yahoo.com>
2023-07-17 14:05:18 +0100

    mail projet rejete

--- resources/views/emails/rejet.blade.php
+++ resources/views/emails/rejet.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <p>M./Mme {{ $projet['user_data']['nom_complet'] }},</p>
     <p>Nous vous informons que votre projet 
-        <a href="{{ route('projet.details', $projet['id']) }}"><strong>{{ $projet['intitule'] }}</strong></a>
+        <a href="https://invest--partners.com/projets/{{$projet['id']}}/details"><strong>{{ $projet['intitule'] }}</strong></a>
             a été rejeté.<br />
             Nous restons à votre écoute au besoin.</p>
     @include('partials.signature')
