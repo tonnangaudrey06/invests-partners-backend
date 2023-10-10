892bfdfbad57431c44bf06f1508b0bd6c2c6eb37
Eddy Harold <donfackeddy12@gmail.com>
2023-10-10 08:18:44 +0100

    Fix update user information bugs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,31 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    /**
-     * @OA\Info(
-     *      version="1.0.0",
-     *      title="Invests & Partners API Documentation",
-     *      description="API version 1.0.0",
-     *      @OA\Contact(
-     *          email="donfackeddy12@gmail.com"
-     *      ),
-     *      @OA\License(
-     *          name="Apache 2.0",
-     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
-     *      )
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST,
-     *      description="Invests & Partners API Documentation"
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST_2,
-     *      description="Invests & Partners API Documentation"
-     * )
-     *
-     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function sendResponse($result, $message)
